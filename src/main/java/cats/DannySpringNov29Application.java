5c359e783a7bebf1a9f2a5b02db0f88a631548ed
Danny Smith <danny@atomist.com>
2018-11-29 15:09:56 +0000

    Initial commit from Atomist

---
+++ src/main/java/cats/DannySpringNov29Application.java
@@ -0,0 +1,12 @@
+package cats;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DannySpringNov29Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(DannySpringNov29Application.class, args);
+	}
+
+}
---
+++ src/main/java/cats/DannySpringNov29Controller.java
@@ -0,0 +1,20 @@
+package cats;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class DannySpringNov29Controller {
+
+	@GetMapping("/")
+	public String root() {
+		return "Hello Kubernetes demo on Wed! I'm served by " + getClass().getName();
+	}
+
+	@GetMapping("hello/{name}")
+	public String person(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+}
